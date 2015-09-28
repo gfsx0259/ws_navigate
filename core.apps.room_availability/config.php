998f10b187e3452a5a81c3b55ded25fdb7ad7b06
konstantin_popov <v8199@yandex.ru>
2015-09-28 15:41:58 +0600

    #913 - Preparation of packages

--- core.apps.room_availability/config.php
+++ core.apps.room_availability/config.php
@@ -6,7 +6,6 @@ $config["js_apps"]["core.apps.room_availability"] = array(
         'title' => 'Room availability',
         'name' => 'room_availability',//should be like 3th part of folder
         'version' => '1.0.0',
-        'icon' => 'icon.png',
         'category' => CATEGORY_NAVIGATE,
         'description' => ''
     ),
